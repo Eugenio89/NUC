04fc95da62eac83426a0f9cb21aba5f725576ac8
Eugenio <sf4e@outlook.com>
2017-05-28 23:54:59 +0900

    Primera Lista de Recetas

---
+++ app/src/main/java/eugenio/nuc/BasicJuices.java
@@ -0,0 +1,13 @@
+package eugenio.nuc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BasicJuices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_juices);
+    }
+}
