bc7b8710f4f8867d9da7b8f74e44f31fb1827bfb
Eugenio <sf4e@outlook.com>
2017-07-12 22:17:19 +0900

    CardView (menu_principal) working, linked to the next Activity

--- app/src/main/java/eugenio/nuc/MainActivity.java
+++ app/src/main/java/eugenio/nuc/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         GetStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Navigation.class));
+                startActivity(new Intent(MainActivity.this, menu_principal.class));
             }
         });
 
